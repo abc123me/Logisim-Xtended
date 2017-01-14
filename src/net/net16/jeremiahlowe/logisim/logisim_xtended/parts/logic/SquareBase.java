8fc81d51db0de3ec05a88df83875839f2d65c3e5
abc123me <jeremiah_abc123me@hotmail.com>
2017-01-14 18:25:36 -0500

    Created new abstract base class

--- src/net/net16/jeremiahlowe/logisim/logisim_xtended/parts/logic/SquareBase.java
+++ src/net/net16/jeremiahlowe/logisim/logisim_xtended/parts/logic/SquareBase.java
@@ -1,57 +1,101 @@
 package net.net16.jeremiahlowe.logisim.logisim_xtended.parts.logic;
 
 import com.cburch.logisim.data.Attribute;
+import com.cburch.logisim.data.AttributeSet;
 import com.cburch.logisim.data.Attributes;
 import com.cburch.logisim.data.BitWidth;
+import com.cburch.logisim.data.Bounds;
 import com.cburch.logisim.data.Direction;
+import com.cburch.logisim.data.Value;
 import com.cburch.logisim.instance.Instance;
 import com.cburch.logisim.instance.InstanceFactory;
-import com.cburch.logisim.instance.InstancePainter;
 import com.cburch.logisim.instance.InstanceState;
 import com.cburch.logisim.instance.Port;
 import com.cburch.logisim.instance.StdAttr;
 
-public class SquareBase extends InstanceFactory{
+import net.net16.jeremiahlowe.logisim.logisim_xtended.instance.StringMaker;
+
+public abstract class SquareBase extends InstanceFactory {
 	private static final Attribute<Integer> INPUTS = Attributes.forIntegerRange("Inputs", 2, 32);
 	public Port[] ports;
 
 	public SquareBase(String name) {
 		super(name);
-		setAttributes(new Attribute[]{
-				StdAttr.WIDTH, StdAttr.LABEL, StdAttr.FACING, INPUTS
-		}, new Object[]{
-				BitWidth.create(1), "", Direction.EAST, 2
+		setAttributes(new Attribute[] { 
+				StdAttr.WIDTH, StdAttr.LABEL, StdAttr.FACING, INPUTS },
+			new Object[] { 
+				BitWidth.create(1), "", Direction.EAST, 2 
 		});
 	}
-
-	@Override
-	public void paintInstance(InstancePainter painter) {
-		// TODO Auto-generated method stub
-		
-	}
+	
+	protected abstract Value doLogicalOperation(Value[] inputs);
 
 	@Override
 	public void propagate(InstanceState state) {
-		// TODO Auto-generated method stub
 		
 	}
-	
+
 	@Override
-	protected void configureNewInstance(Instance instance){
+	public Bounds getOffsetBounds(AttributeSet attrs) {
+		int x = 0;
+		int y = 0;
+		int w = 30;
+		int h = attrs.getValue(INPUTS) * 10 + 10;
+		if((h / 10) % 2 != 0) h += 10;
+		return Bounds.create(x, y, w, h).rotate(Direction.EAST, attrs.getValue(StdAttr.FACING), x, y);
+	}
+
+	@Override
+	protected void configureNewInstance(Instance instance) {
 		instance.addAttributeListener();
 		initPorts(instance);
 		instance.recomputeBounds();
 	}
-	
+
+	@Override
+	protected void instanceAttributeChanged(Instance instance, Attribute<?> attr) {
+		if(attr != StdAttr.LABEL){
+			initPorts(instance);
+			instance.recomputeBounds();
+		}
+	}
+
 	private void initPorts(Instance instance){
 		int inputPorts = instance.getAttributeValue(INPUTS);
+		int bitWidth = instance.getAttributeValue(StdAttr.WIDTH).getWidth();
+		EDirection facing = EDirection.convert(instance.getAttributeValue(StdAttr.FACING));
+		Bounds bds = instance.getBounds();
 		ports = new Port[inputPorts + 1];
-		int x = 0;
-		int y = 0;
-		ports[0] = new Port(x, y, Port.OUTPUT, instance.getAttributeValue(StdAttr.WIDTH));
+		int xm = 0, ym = 0; 
+		boolean flip = false;
+		switch(facing){
+			case EAST: xm = 1; ym = 1; flip = false; break; 
+			case WEST: xm = -1; ym = -1; flip = false; break;
+			case NORTH: xm = -1; ym = 1; flip = true; break;
+			case SOUTH: xm = 1; ym = -1; flip = true; break;
+		}
+		
+		int x = xm * bds.getWidth();
+		int y = ym * (bds.getHeight() / 2);
+		ports[0] = new Port(flip ? y : x, flip ? x : y, Port.OUTPUT, bitWidth);
+		ports[0].setToolTip(new StringMaker("Output"));
+		
 		for(int i = 1; i < ports.length; i++){
+			int tmpX = xm * 0;
+			int tmpY = ym * (i * 10);
 			
+			ports[i] = new Port(flip ? tmpY : tmpX, flip ? tmpX : tmpY, Port.INPUT, bitWidth);
+			ports[i].setToolTip(new StringMaker("Input " + i));
 		}
 		instance.setPorts(ports);
+	}
+	private static enum EDirection{
+		NORTH, SOUTH, EAST, WEST;
+		public static EDirection convert(Direction in){
+			if(in == Direction.WEST) return WEST;
+			else if(in == Direction.NORTH) return NORTH;
+			else if(in == Direction.SOUTH) return SOUTH;
+			else return EAST;
+		}
 	}
 }
